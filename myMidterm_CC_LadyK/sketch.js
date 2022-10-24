0ae1c569d44c31b925301e863d57c370e6141639
LadyK <LadyK@users.noreply.github.com>
2022-10-24 14:53:04 -0400

    first push

---
+++ myMidterm_CC_LadyK/sketch.js
@@ -0,0 +1,32 @@
+function setup() {
+  createCanvas(800, 800);
+  background(0);
+}
+
+function draw() {
+ 
+
+}
+
+function mousePressed(){
+ 
+ /*
+  print(getRectArea(3, 4));
+
+  print(getRectArea(-3, 4));
+  */
+
+}
+
+/*
+function getRectArea(width, height) {
+  if (width > 0 && height > 0) {
+    return width * height;
+  }
+  return 0;
+}
+*/
+
+
+
+
